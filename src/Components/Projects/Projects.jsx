1e76b84b909ab8f88b579e4e23ca8cffb3ef4026
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2024-08-27 19:39:30 -0400

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -30,7 +30,6 @@ export const Projects = () => {
                   <SiAlteryx />
                   <SiAzurefunctions />
                   <SiMicrosoftazure />
-                  <DiDatabase />
                   <SiPowerbi/>
                   
                 
