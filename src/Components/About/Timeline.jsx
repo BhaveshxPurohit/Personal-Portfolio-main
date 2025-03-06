b171a948d1a51d90026a29b1aa06c4433b8b6869
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-03-06 15:55:10 -0500

    Update Timeline.jsx

--- src/Components/About/Timeline.jsx
+++ src/Components/About/Timeline.jsx
@@ -55,7 +55,7 @@ export const Timeline = () => {
               className="vertical-timeline-element-title"
               data-aos="fade-right"
             >
-              Data Scientist
+              AI Engineer
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
@@ -124,7 +124,7 @@ export const Timeline = () => {
               className="vertical-timeline-element-title"
               data-aos="fade-right"
             >
-              Data Analyst
+              System Administrator (L3)
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
@@ -137,7 +137,7 @@ export const Timeline = () => {
             </p>
           </VerticalTimelineElement>
 <VerticalTimelineElement
-            date={"Jul 2020 - Jun 2022"}
+            date={"Jul 2021 - Jun 2022"}
             contentStyle={{
               boxShadow: `var(--shadow)`,
               border: "3px solid var(--clr-primary)",
