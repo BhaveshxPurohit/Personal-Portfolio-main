f106bb79d3820f38edad7cd5df08928482965ae2
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-04-12 15:58:29 -0400

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -55,7 +55,7 @@ export const About = () => {
             <GitHubIcon />
           </a>
           <a
-            href="mailto:bppurohi@gmail.cpm"
+            href="mailto:purohitbhavesh912@gmail.com"
             target="_blank"
             rel="noreferrer"
             aria-label="mail"
@@ -64,7 +64,7 @@ export const About = () => {
             <EmailIcon />
           </a>
           <a
-            href="tel:+16803568409"
+            href="tel:+13154968715"
             target="_blank"
             rel="noreferrer"
             aria-label="phone"
