0f583139c0d654c0ec20ca1903ef4d6913616986
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-06-15 21:23:43 -0400

    Update Timeline.jsx

--- src/Components/About/Timeline.jsx
+++ src/Components/About/Timeline.jsx
@@ -103,40 +103,6 @@ export const Timeline = () => {
             </p>
           </VerticalTimelineElement>
            <VerticalTimelineElement
-            date={"Jun 2024 - Aug 2024"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              textAlign: "center",
-              color: `var(--clr-fg-alt)`,
-            }}
-            contentArrowStyle={{
-              borderRight: "16px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: ` 3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<WorkIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              Data Analyst Intern
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              data-aos="fade-right"
-            >
-              <p>
-              CLAB AI
-               </p>
-            </h4>
-          </VerticalTimelineElement>
-           <VerticalTimelineElement
             date={"Aug 2024 - Dec 2024"}
             contentStyle={{
               boxShadow: `var(--shadow)`,
@@ -170,7 +136,40 @@ export const Timeline = () => {
                </p>
             </h4>
           </VerticalTimelineElement>
-         
+                    <VerticalTimelineElement
+            date={"Jun 2024 - Aug 2024"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "16px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<WorkIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              Data Analyst Intern
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              <p>
+              CLAB AI
+               </p>
+            </h4>
+          </VerticalTimelineElement>
           <VerticalTimelineElement
             date={"Jul 2022 - Jun 2023"}
             contentStyle={{
