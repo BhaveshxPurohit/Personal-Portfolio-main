0d4de797580271a101c78bab333cf5e19e39b7d3
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-06-23 15:29:47 -0400

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -86,7 +86,7 @@ export const About = () => {
           className="btnResume"
           onClick={() => {
             window.open(
-              "https://drive.google.com/file/d/1uN7AXhOgq-utHEMW3-YnRyGnrnxzT_mV/view?usp=sharing"
+              "https://drive.google.com/file/d/1NzLhbgpXKh_difAOjVhLoK6FSD6tGqEI/view?usp=sharing"
             );
           }}
         >
