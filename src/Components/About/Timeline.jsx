909fff88e78c3f32dd32effa45ed1d79028e5abc
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-07-30 13:24:58 -0400

    Update Timeline.jsx

--- src/Components/About/Timeline.jsx
+++ src/Components/About/Timeline.jsx
@@ -32,40 +32,6 @@ export const Timeline = () => {
         </h2>
         <VerticalTimeline lineColor={linecolor}>
            <VerticalTimelineElement
-            date={"Jun 2024 - Present"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              textAlign: "center",
-              color: `var(--clr-fg-alt)`,
-            }}
-            contentArrowStyle={{
-              borderRight: "16px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: ` 3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<WorkIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              Data Analyst Associate
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              data-aos="fade-right"
-            >
-              <p>
-              Goldman Sachs 
-               </p>
-            </h4>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
             date={"Aug 2023 - May 2025"}
             contentStyle={{
               boxShadow: `var(--shadow)`,
