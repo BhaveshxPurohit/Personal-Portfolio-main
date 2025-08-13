d28ef5cc7f58f858799af8454cf1e15ca3050f63
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-08-13 17:29:15 -0400

    Update Timeline.jsx

--- src/Components/About/Timeline.jsx
+++ src/Components/About/Timeline.jsx
@@ -54,7 +54,7 @@ export const Timeline = () => {
               className="vertical-timeline-element-title"
               data-aos="fade-right"
             >
-              Student
+              Financial Data Analyst
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
