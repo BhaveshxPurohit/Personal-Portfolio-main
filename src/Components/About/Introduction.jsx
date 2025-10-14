93f3e2456a9bce031a8d752e08e184b4e35bcace
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-10-14 11:26:36 -0400

    Update Introduction.jsx

--- src/Components/About/Introduction.jsx
+++ src/Components/About/Introduction.jsx
@@ -18,7 +18,7 @@ export const Introduction = () => {
             <img src={profilePic} alt="Bhavesh Purohit" />
           </div>
           <div className="introduction_datacontainer">
-            <p> Full-Stack & Data Engineer | Expert in Python, SQL, Spark, and Cloud-Native Architecture for High-Performance ETL, API, and Analytics Systems. </p>
+            <p> Full-Stack & Data Engineer with 3 Years of expertise in Python, SQL, Spark, and Cloud-Native Architecture for High-Performance ETL, API, and Analytics Systems. </p>
             <p> My journey began with curiosity for how data moves, scales, and powers decisions, and evolved into a passion for building cloud-native architectures, ETL pipelines, and API-driven ecosystems that make technology faster, smarter, and more reliable. I’ve engineered solutions across the stack using Python, SQL, Spark, Django, React.js, Next.js, and Kubernetes, deploying resilient applications on AWS and GCP.</p>
             <p>I thrive at the intersection of software engineering and data analytics, where I can design microservices that process millions of records, optimize API throughput, and enable real-time insights for business and financial systems. Whether refining performance bottlenecks or automating workflows, my focus remains the same, building solutions that scale with impact and precision.</p>
             <p>Beyond code, I’m deeply motivated by continuous learning and the pursuit of technical excellence, exploring modern frameworks, experimenting with distributed systems, and collaborating with teams who value innovation and measurable outcomes. I’m always open to connecting with professionals who share a vision for using data and software to solve real-world challenges and drive meaningful change.</p>
