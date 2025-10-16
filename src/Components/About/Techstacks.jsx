6a9f8a8a78ca144ce54609355d4e321bebf78b48
Bhavesh Purohit <56186146+BhaveshxPurohit@users.noreply.github.com>
2025-10-16 11:50:15 -0400

    Update Techstacks.jsx

--- src/Components/About/Techstacks.jsx
+++ src/Components/About/Techstacks.jsx
@@ -1,84 +1,62 @@
 import React from "react";
-import {
-  SiPython, SiJavascript, SiTypescript, SiCplusplus, SiC, SiDjango, SiFlask,
-  SiReact, SiNextdotjs, SiNodedotjs, SiSpringboot, SiExpress, SiAmazonaws,
-  SiGooglecloud, SiDocker, SiKubernetes, SiJenkins, SiGit, SiGithub, SiGitlab,
-  SiTerraform, SiApachekafka, SiMysql, SiPostgresql, SiMongodb, SiOracle,
-  SiGraphql, SiRedux, SiBootstrap, SiHtml5, SiCss3, SiJquery, SiFastapi,
-  SiJira, SiPostman, SiPytest,
-} from "react-icons/si";
-import { FaJava } from "react-icons/fa";
-import { PiFileSql, PiFileCode } from "react-icons/pi";
+import { SiPython, SiJava, SiJavascript, SiTypescript, SiCplusplus,
+         SiDjango, SiFlask, SiReact, SiNextdotjs, SiNodedotjs, SiSpringboot,
+         SiAmazonaws, SiGooglecloud, SiMicrosoftazure, SiDocker, SiKubernetes,
+         SiJenkins, SiGitlab, SiTerraform, SiMysql, SiPostgresql, SiMongodb,
+         SiOracle, SiSnowflake, SiApachespark, SiApacheairflow } from "react-icons/si";
 import "./Techstacks.css";
 
 const categories = [
   {
-    title: "Programming Languages",
+    title: "💻 Languages",
     techs: [
-      { icon: <SiC />, name: "C" },
-      { icon: <SiCplusplus />, name: "C++" },
       { icon: <SiPython />, name: "Python" },
-      { icon: <FaJava />, name: "Java" },
+      { icon: <SiJava />, name: "Java" },
       { icon: <SiJavascript />, name: "JavaScript" },
       { icon: <SiTypescript />, name: "TypeScript" },
-      { icon: <PiFileSql />, name: "SQL" },
-      { icon: <SiMongodb />, name: "NoSQL" },
+      { icon: <SiCplusplus />, name: "C++" },
+      { icon: <SiMysql />, name: "SQL" }, // Use MySQL icon as SQL
     ],
   },
   {
-    title: "Frontend",
-    techs: [
-      { icon: <SiHtml5 />, name: "HTML5" },
-      { icon: <SiCss3 />, name: "CSS3" },
-      { icon: <SiJquery />, name: "jQuery" },
-      { icon: <PiFileCode />, name: "XML/JSON" },
-      { icon: <SiReact />, name: "React.js" },
-      { icon: <SiNextdotjs />, name: "Next.js" },
-      { icon: <SiBootstrap />, name: "Bootstrap" },
-      { icon: <SiRedux />, name: "Redux" },
-      { icon: <SiFastapi />, name: "FastAPI" },
-    ],
-  },
-  {
-    title: "Backend",
+    title: "🧰 Frameworks & Libraries",
     techs: [
       { icon: <SiDjango />, name: "Django" },
       { icon: <SiFlask />, name: "Flask" },
-      { icon: <SiSpringboot />, name: "Spring Boot" },
+      { icon: <SiReact />, name: "React" },
+      { icon: <SiNextdotjs />, name: "Next.js" },
       { icon: <SiNodedotjs />, name: "Node.js" },
-      { icon: <SiExpress />, name: "Express.js" },
+      { icon: <SiSpringboot />, name: "Spring Boot" },
     ],
   },
   {
-    title: "Cloud",
+    title: "☁️ Cloud & Infrastructure",
     techs: [
       { icon: <SiAmazonaws />, name: "AWS" },
       { icon: <SiGooglecloud />, name: "GCP" },
+      { icon: <SiMicrosoftazure />, name: "Azure" },
     ],
   },
   {
-    title: "Tools & Technologies",
+    title: "🐳 DevOps & Tools",
     techs: [
-      { icon: <SiPytest />, name: "PyTest" },
-      { icon: <SiPostman />, name: "Postman" },
-      { icon: <SiJira />, name: "Jira" },
       { icon: <SiDocker />, name: "Docker" },
       { icon: <SiKubernetes />, name: "Kubernetes" },
-      { icon: <SiGit />, name: "Git" },
-      { icon: <SiGithub />, name: "GitHub" },
-      { icon: <SiGitlab />, name: "GitLab" },
+      { icon: <SiJenkins />, name: "Jenkins" },
+      { icon: <SiGitlab />, name: "GitLab CI" },
       { icon: <SiTerraform />, name: "Terraform" },
-      { icon: <SiApachekafka />, name: "Kafka" },
-      { icon: <SiGraphql />, name: "GraphQL" },
+      { icon: <SiApacheairflow />, name: "Airflow" },
     ],
   },
   {
-    title: "Database",
+    title: "🗄️ Databases & Analytics",
     techs: [
       { icon: <SiMysql />, name: "MySQL" },
       { icon: <SiPostgresql />, name: "PostgreSQL" },
       { icon: <SiMongodb />, name: "MongoDB" },
       { icon: <SiOracle />, name: "Oracle" },
+      { icon: <SiSnowflake />, name: "Snowflake" },
+      { icon: <SiApachespark />, name: "Spark" },
     ],
   },
 ];
@@ -94,7 +72,7 @@ export const Techstacks = () => {
             {cat.techs.map((tech, index) => (
               <div className="tech-item" key={index}>
                 {tech.icon}
-                <p>{tech.name}</p>
+                <span>{tech.name}</span>
               </div>
             ))}
           </div>
